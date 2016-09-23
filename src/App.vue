86298f28868d57f7d5ab228bacc8af3a71025027
Federico Marcos <marcosfede@gmail.com>
2016-09-22 22:07:25 -0300

    added double operator, rounding to 9 digits. added keyboard shortcuts added clear on new calculation or reuse last result. TODO: change display size and digits  stored based on results length?

--- src/App.vue
+++ src/App.vue
@@ -1,160 +1,289 @@
 <template>
-  <div id="app">
+<div id="app" v-on:keyup="handleClick">
     <div id="top-bar"></div>
     <div id="screen">
-      <span>{{current}}</span>
+        <span>{{block}}</span>
+    </div>
+    <div id="screen">
+        <span>{{current }}</span>
     </div>
     <div id="cal-body">
-      <div class="row" id="first-row">
-      <Key :clck="clck" class="button" value="c"></Key>
-      <Key :clck="clck" class="button" value="±"></Key>
-      <Key :clck="clck" class="button" value="%"></Key>
-      <Key :clck="clck" class="button operator" value="÷"></Key>
+        <div class="row" id="first-row">
+            <Key :clck="handleClick" class="button" :value="current ? 'C' : 'AC'"></Key>
+            <Key :clck="handleClick" class="button" value="pm"></Key>
+            <Key :clck="handleClick" class="button" value="%"></Key>
+            <Key :clck="handleClick" class="button operator" value="/"></Key>
+        </div>
+        <div class="row" id="second-row">
+            <Key :clck="handleClick" class="button" value="7"></Key>
+            <Key :clck="handleClick" class="button" value="8"></Key>
+            <Key :clck="handleClick" class="button" value="9"></Key>
+            <Key :clck="handleClick" class="button operator" value="*"></Key>
+        </div>
+        <div class="row" id="third-row">
+            <Key :clck="handleClick" class="button" value="4"></Key>
+            <Key :clck="handleClick" class="button" value="5"></Key>
+            <Key :clck="handleClick" class="button" value="6"></Key>
+            <Key :clck="handleClick" class="button operator" value="-"></Key>
+        </div>
+        <div class="row" id="fourth-row">
+            <Key :clck="handleClick" class="button" value="1"></Key>
+            <Key :clck="handleClick" class="button" value="2"></Key>
+            <Key :clck="handleClick" class="button" value="3"></Key>
+            <Key :clck="handleClick" class="button operator" value="+"></Key>
+        </div>
+        <div class="row" id="fifth-row">
+            <Key :clck="handleClick" class="button long" value="0"></Key>
+            <Key :clck="handleClick" class="button" value="."></Key>
+            <Key :clck="handleClick" class="button operator" value="="></Key>
+        </div>
     </div>
-    <div class="row" id="second-row">
-      <Key :clck="clck" class="button" value="7"></Key>
-      <Key :clck="clck" class="button" value="8"></Key>
-      <Key :clck="clck" class="button" value="9"></Key>
-      <Key :clck="clck" class="button operator" value="×"></Key>
-    </div>
-    <div class="row" id="third-row">
-      <Key :clck="clck" class="button" value="4"></Key>
-      <Key :clck="clck" class="button" value="5"></Key>
-      <Key :clck="clck" class="button" value="6"></Key>
-      <Key :clck="clck" class="button operator" value="-"></Key>
-    </div>
-    <div class="row" id="fourth-row">
-      <Key :clck="clck" class="button" value="1"></Key>
-      <Key :clck="clck" class="button" value="2"></Key>
-      <Key :clck="clck" class="button" value="3"></Key>
-      <Key :clck="clck" class="button operator" value="+"></Key>
-    </div>
-    <div class="row" id="fifth-row">
-      <Key :clck="clck" class="button long" value="0"></Key>
-      <Key :clck="clck" class="button" value="."></Key>
-      <Key :clck="clck" class="button operator" value="="></Key>
-    </div>
-    </div>
-  </div>
+</div>
 </template>
 
 <script>
 import Key from './components/Key.vue'
 export default {
 
-  name: 'App',
+    name: 'App',
 
-  components: {
-    Key
-  },
+    created: function (){
+      window.addEventListener('keyup', this.handleKey)
+    },
 
-  data () {
-    return {
-      block: '',
-      current: ''
-    }
-  },
+    components: {
+        Key
+    },
 
-  methods: {
-      clck : function (k) {
-        console.log(k)
-        switch (k) {
-          case '=':
-            this.current = this.block = this.block + this.current
-            break
-          case 'c':
-            this.current = this.block = ''
-            break
-          case '±':
-            this.current = "-"+this.current
-            break
-          case '%':
-            this.block += k
-            this.current = ''
-            break
-          case '+':
-            this.block += k
-            this.current = ''
-            break
-          case '-':
-            this.block += k
-            this.current = ''
-            break
-          case '×':
-            this.block += k
-            this.current = ''
-            break
-          case '÷':
-            this.block += k
-            this.current = ''
-            break
-          default:
-            this.current += k
+    data() {
+        return {
+            block: '',
+            current: '0',
+            lastkey: null
         }
-    }
-  }
-}
+    },
 
+    watch : {
+      current : function (val) {
+        if (val.length > 9) { this.current = val.slice(0,9)}
+      }
+    },
+
+    methods: {
+        handleKey : function(key){
+          let k = key.key
+          switch (k){
+              case 'Enter':
+              case '=':
+                  this.Equal()
+                  break
+              case 'Backspace':
+                  this.Clear('C')
+                  break
+              // case 'AC':
+              //     this.Clear(k)
+              //     break
+              // case 'pm':
+              //     this.inputPM()
+              //     break
+              case '%':
+                  this.inputPercent()
+                  break
+              case '+':
+              case '-':
+              case '*':
+              case '/':
+                  this.inputOperator(k)
+                  break
+              case '.':
+                  this.inputDot()
+                  break
+              case '1':
+              case '2':
+              case '3':
+              case '4':
+              case '5':
+              case '6':
+              case '7':
+              case '8':
+              case '9':
+              case '0':
+                  this.inputDigit(k)
+                  break
+          }
+        },
+        handleClick: function(k) {
+            switch (k) {
+                case '=':
+                    this.Equal()
+                    break
+                case 'C':
+                case 'AC':
+                    this.Clear(k)
+                    break
+                case 'pm':
+                    this.inputPM()
+                    break
+                case '%':
+                    this.inputPercent()
+                    break
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                    this.inputOperator(k)
+                    break
+                case '.':
+                    this.inputDot()
+                    break
+                default:
+                    this.inputDigit(k)
+            }
+        },
+
+        evaluate: function(num) {
+            return (+eval(num).toFixed(8)).toString()
+        },
+
+        inputOperator: function(operator) {
+            if (this.current === '') {
+              this.block = this.block.slice(0,-1)+operator
+            }
+            else {
+                this.block += '(' + this.current + ')' + operator
+                this.current = ''
+            }
+            this.lastkey = operator
+        },
+
+        inputDigit: function(digit) {
+            if (this.lastkey === '=') this.current = ''
+            if (digit === '0') {
+                if (this.current !== '0') {
+                    this.current += digit
+                }
+            } else if (this.current === '0') {
+                this.current = digit
+            } else {
+                this.current += digit
+            }
+            this.lastkey = digit
+        },
+
+        inputDot: function() {
+            if (this.current.indexOf('.') === -1) {
+                this.current += '.'
+            }
+            this.lastkey = '.'
+        },
+
+        inputPercent: function() {
+            if (this.block === '') {
+                this.current = this.evaluate(this.current + '/100')
+            } else {
+                this.current = this.evaluate(this.block.slice(0, -1) + '*' + this.current + '/100')
+            }
+            this.lastkey = '%'
+        },
+
+        inputPM: function() {
+            if (this.current[0] === '-') {
+                this.current = this.current.slice(1)
+            } else {
+                this.current = "-" + this.current
+            }
+            this.lastkey = 'pm'
+        },
+
+        Clear: function(key) {
+            if (key === 'C') this.current = '0'
+            else if (key === 'AC') this.block = ''
+            this.lastkey = key
+        },
+
+        Equal: function() {
+            this.block += '(' + this.current + ')'
+            console.log(this.block)
+            this.current = this.evaluate(this.block)
+            this.block = ''
+            this.lastkey = '='
+        },
+    }
+}
 </script>
 
 <style>
-body, html {
-  height: 100%;
-  font: 100 14px 'Roboto';
-  background-color: #ddd;
+body,
+html {
+    height: 100%;
+    font: 100 14px 'Roboto';
+    background-color: #ddd;
 }
+
 #app {
-  width: 320px;
-  margin: 100px auto auto auto;
-  display: flex;
-  flex-direction: column;
-  background: black;
-  box-shadow: 0px 0px 20px 0px #aaa;
+    width: 320px;
+    margin: 100px auto auto auto;
+    display: flex;
+    flex-direction: column;
+    background: black;
+    box-shadow: 0px 0px 20px 0px #aaa;
 }
+
 #screen {
-  color: white;
-  display: flex;
-  justify-content: flex-end;
-  align-items: flex-end;
+    color: white;
+    display: flex;
+    justify-content: flex-end;
+    align-items: flex-end;
+    height: 100px;
+    width: 320px;
+    background-color: #202020;
 }
+
 #screen>span {
-  font-size: 4em;
+    padding-right: 18px;
+    padding-bottom: 5px;
+    font-size: 4em;
 }
-div.row {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
+
+#cal-body {
+    background-color: #E0E0E0;
 }
-.button {
-  width: 80px;
-  height: 80px;
-  background-color: #D2D3D5;
-  font: 100 250% 'Roboto';
-  border: none;
-  padding: 0;
-  font-family: inherit;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-top: 1px solid #777;
-  border-right: 1px solid #777;
-}
-.button:active {
-  box-shadow: inset 0px 0px 80px 0px rgba(0,0,0,0.25);
-}
-.button.long {
-  width: 160px;
-}
-#screen {
-  height: 120px;
-  width: 320px;
-  background-color: #202020
-}
+
 #first-row {
-  background-color: #C7C8CA;
+    background-color: #D6D6D6;
 }
+
+.row {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+}
+
+.button {
+    width: 80px;
+    height: 80px;
+    background-color: inherit;
+    font: 100 250% 'Roboto';
+    border: none;
+    padding: 0;
+    font-family: inherit;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-top: 1px solid #777;
+    border-right: 1px solid #777;
+}
+
+.button:active {
+    box-shadow: inset 0px 0px 80px 0px rgba(0, 0, 0, 0.25);
+}
+
+.button.long {
+    width: 160px;
+}
+
 .button.operator {
-  background-color: #F28310;
-  color: white;
+    background-color: #F28310;
+    color: white;
 }
 </style>
--- src/components/Key.vue
+++ src/components/Key.vue
@@ -1,23 +1,36 @@
 <template>
-<button @click="clck(value)">
-<slot>{{value}}</slot>
+<button  @click="clck(value)">
+<slot>{{valuetobutton(value)}}</slot>
 </button>
 </template>
 
 <script>
 export default {
-
   name: 'Key',
   props: ['value', 'clck'],
-  data () {
-    return {
-    }
-  },
   methods : {
     clicked : function () {
       this.$emit(this.value)
+    },
+    valuetobutton : function (value) {
+      switch (value) {
+        case '*':
+          return '×'
+        case '/':
+          return '÷'
+        case 'pm':
+          return '±'
+        default:
+          return value
+      }
     }
   }
 }
 
 </script>
+<style>
+button {
+  background-color: inherit;
+}
+button:focus {outline:0;}
+</style>
